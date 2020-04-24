383524cae9f7ae3c841eded211634e7bac399410
azabraao <az.abraao@gmail.com>
2020-04-24 15:59:28 -0300

    Add testimonials

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $(".jsSlide").slick({
+    dots: true,
+    prevArrow: $(".jsSlidePrev"),
+    nextArrow: $(".jsSlideNext"),
+  });
+
+  var qtdSlides = $("[id^=slick-slide0]").length;
+  var slideAtual = 1;
+
+  $(".jsSlide").on("afterChange", function (event, slick, direction) {
+    $(".jsSlideSteps").text(direction + 1 + "/" + qtdSlides);
+  });
+
+  $(".jsSlideSteps").text(slideAtual + "/" + qtdSlides);
+
+  $(".jsSlidePrev").on("click", function () {
+    $(this).addClass("active");
+    $(".jsSlideNext").removeClass("active");
+  });
+
+  $(".jsSlideNext").on("click", function () {
+    $(this).addClass("active");
+    $(".jsSlidePrev").removeClass("active");
+  });
+});
