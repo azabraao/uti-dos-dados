724e4977c6efe15430a5147147ff30db0db0fb3d
azabraao <az.abraao@gmail.com>
2020-04-29 16:22:12 -0300

    Rename title

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <!-- TODO: Rename title -->
-    <title>Document</title>
+    
+    <title>UTI dos dados</title>
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <link
